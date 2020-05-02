73284f4df41faf2c1b648b16e7105fde41aa7124
= <=>
2020-05-02 17:43:01 +0200

    edit test file

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -2,11 +2,16 @@ import pytest
 import fibonacci
 import random, string
 
-
-def test_fibonacci():
-    assert 1==1
-
 def test_add_Node():
     result = fibonacci.Node(0)
     assert result.data == 0
     assert repr(result.next)== 'None'
+
+def test_add_list():
+    linklist = fibonacci.LinkedList()
+    assert repr(linklist.head) == 'None'
+
+
+def test_first_list():
+    node = fibonacci.Node(0)
+    repr(fibonacci.LinkedList().add_first(node))
